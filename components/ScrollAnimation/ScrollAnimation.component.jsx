f6f6f5b726d590c55b9221a040c0749a59c08b22
charanpy <durgicharan81@example.com>
2021-07-18 21:01:44 +0530

    added project detail page

--- components/ScrollAnimation/ScrollAnimation.component.jsx
+++ components/ScrollAnimation/ScrollAnimation.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { motion, useAnimation } from 'framer-motion';
 
@@ -7,27 +7,39 @@ const centerClass = { display: 'flex', justifyContent: 'center' };
 const ScrollAnimation = ({ children, center = false }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView();
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    if (typeof window !== undefined) {
+      if (window.innerWidth <= 800) {
+        setIsMobile(true);
+      }
+    }
     if (inView) {
       controls.start('visible');
     }
   }, [controls, inView]);
 
   return (
-    <motion.div
-      style={center ? centerClass : ''}
-      ref={ref}
-      animate={controls}
-      initial='hidden'
-      transition={{ duration: 0.5 }}
-      variants={{
-        visible: { opacity: 1, scale: 1 },
-        hidden: { opacity: 0, scale: 0 },
-      }}
-    >
-      {children}
-    </motion.div>
+    <>
+      {isMobile ? (
+        <div>{children}</div>
+      ) : (
+        <motion.div
+          style={center ? centerClass : ''}
+          ref={ref}
+          animate={controls}
+          initial='hidden'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </>
   );
 };
 
