32e4b704e1f70fc5e891b74f230c804c125beaa9
charanpy <durgicharan81@example.com>
2021-07-18 20:51:47 +0530

    added project detail page

--- layout/PageTransition/PageTransition.component.jsx
+++ layout/PageTransition/PageTransition.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0, delay: 1, duration: 5 },
-  exit: { opacity: 0, x: 0, y: -100, duration: 5 },
+  hidden: { opacity: 0 },
+  enter: { opacity: 1, delay: 1, duration: 5 },
+  exit: { opacity: 0, duration: 5 },
 };
 const PageTransition = ({ children, route }) => {
   return (
