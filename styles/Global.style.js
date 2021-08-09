e3dabe02a53c255ebe1831fdcffa846625a20279
charanpy <durgicharan81@example.com>
2021-08-09 21:20:10 +0530

    added color

--- styles/Global.style.js
+++ styles/Global.style.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 html {
 color: ${(props) => props.theme.color} !important;
-      background: ${(props) => props.theme.background} !important;
+      background-color: ${(props) => props.theme.background} !important;
 }
   body {
       
